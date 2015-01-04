97c14ef0a9f4fcdbdd4ee357b5fbf6364df22e46
zhou <zhouworker@gmail.com>
2015-01-04 20:29:39 +0800

    添加csrf功能

--- src/main/java/com/zblog/common/util/constants/Constants.java
+++ src/main/java/com/zblog/common/util/constants/Constants.java
@@ -1,14 +1,18 @@
 package com.zblog.common.util.constants;
 
 public final class Constants{
-  private Constants(){}
-  
+  private Constants(){
+  }
+
   public static final String APPLICATION_NAME = "JavaTalk-赚够钱背着画板去旅行";
   public static String DOMAIN = "http://www.zhc.com";
-  
-  public static final String  ENCODING_UTF_8              = "UTF-8";
-  
-  public static final String  COOKIE_CONTEXT_ID              = "c_id";
-  public static final String  COOKIE_USER_NAME               = "un";
-  
+
+  public static final String ENCODING_UTF_8 = "UTF-8";
+
+  public static final String COOKIE_CONTEXT_ID = "c_id";
+  public static final String COOKIE_USER_NAME = "un";
+
+  public static final String CRSF_TOKEN = "CSRFToken";
+  public static final String AJAX_CRSF_TOKEN = "X-CSRFToken";
+
 }
